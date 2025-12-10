8811f0cca82e95bec9bf7e081f19ecb600678796
Muhammed Arshad <muhammedmcarshad@gmail.com>
2025-12-10 20:38:17 +0530

    leetcode

---
+++ 283.dart
@@ -0,0 +1,24 @@
+class Solution {
+  void moveZeroes(List<int> nums) {
+    int j = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        j++;
+      }
+    }
+
+    // ✅ Print the result
+    print(nums);
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+
+  List<int> nums = [0, 1, 0, 3, 12];
+  sol.moveZeroes(nums);   // Output: [1, 3, 12, 0, 0]
+}
