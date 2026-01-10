39ddd5fc8bc82abfb8296422b98224a6746ebecc
Muhammed Arshad <muhammedmcarshad@gmail.com>
2026-01-10 11:49:49 +0530

    updated

--- test.dart
+++ test.dart
@@ -1,13 +1,18 @@
-void main(){
-  List<int> numbers = [1,2,3,4,5,6];
+
+void oddnumbersFind(List<int> nums){
   List<dynamic> result = [];
 
-  for(int i=0 ; i<numbers.length ; i++){
-    if(numbers[i] %2 == 1){
-      result.add('Odd');
+  for(int i=0 ; i<nums.length; i++){
+    if(nums[i] %2 == 0){
+      result.add('Even');
     }else{
-      result.add(numbers[i]);
+      result.add(nums[i]);
     }
   }
   print(result);
+}
+
+void main(){
+  List<int> numbers = [1,2,3,4,5,6];
+  oddnumbersFind(numbers);
 }
